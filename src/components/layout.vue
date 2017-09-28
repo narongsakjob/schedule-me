80535d84596d7d8d18f9e5dacf23503d1f2efc8d
Narongsak <narongsak.chobsri@gmail.com>
2017-09-28 22:35:11 +0700

    Divide layout

---
+++ src/components/layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="main-bar">asdads</div>
+    <div id="side-bar">asddsa</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout',
+  data () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#main-bar {
+  float: left;
+	width: 70%;
+}
+
+#side-bar {
+  float: right;
+	width: 30%;
+}
+
+</style>
